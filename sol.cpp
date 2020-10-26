0faeb4fe0899e436f20ef308ff0bb5cf5763e943
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-10-26 10:25:15 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	cin >> s;
+	string tmp = "WUB";
+	for (int i = 0; i < (int) s.size(); i++) {
+		if (s[i] == 'W' && s[i + 1] == 'U' && s[i + 2] == 'B') {
+			i += 2;
+			cout << " ";
+		} else {
+			cout << s[i];
+		}
+	}
+	cout << '\n';
+	return 0;
+}
